20213adc01620c32892bef5af10b9fc23a0341c9
ktr0731 <aizu.s1230022@gmail.com>
2018-04-20 15:01:56 +0900

    up

--- test.rb
+++ test.rb
@@ -3,5 +3,5 @@ class Test < Formula
   homepage "https://github.com/ktr0731"
   url "https://github.com/ktr0731/foo/releases/download/foo.tar.gz"
   sha256 "76087d83df671ce4fb80681e179819a4363f8a6e94a804aa094dfa21bfd90d30"
-  version "0.1.0"
+  version "0.1.1"
 end
