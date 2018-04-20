728489e19b605a7aa784cb983c2644c66dbb38be
ktr0731 <aizu.s1230022@gmail.com>
2018-04-20 15:00:17 +0900

    init

---
+++ test.rb
@@ -0,0 +1,7 @@
+class Evans < Formula
+  desc "test"
+  homepage "https://github.com/ktr0731"
+  url "https://github.com/ktr0731/foo/releases/download/foo.tar.gz"
+  sha256 "76087d83df671ce4fb80681e179819a4363f8a6e94a804aa094dfa21bfd90d30"
+  version "0.1.0"
+end
